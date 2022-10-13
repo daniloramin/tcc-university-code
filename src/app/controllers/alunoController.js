de933da32774c4f9927b89f89b9425f8ed124821
daniloramin <daniloramin00@hotmail.com>
2022-10-13 14:35:18 -0300

    mudou o nome das rotas para aluno e finalizou a rota que pega todas as informacoes

--- src/app/controllers/alunoController.js
+++ src/app/controllers/alunoController.js
@@ -4,6 +4,8 @@ const update = async (req, res) => {
   const { teste, nome, sobrenome, telefone, email_de_contato } = req.body;
   const { _id, hierarquia } = req.body.payload;
 
+  // if hierarquia == professor return error?
+
   try {
     const verification = await Aluno.findOne({ "conta._id": _id });
 
@@ -21,10 +23,57 @@ const update = async (req, res) => {
       { new: true }
     );
 
-    return res.status(200).send({ message: "Aluno atualizado", aluno });
+    return res.status(200).send({ message: "Aluno atualizado.", aluno });
   } catch (err) {
     return res.status(400).send({ message: err.message });
   }
 };
 
-module.exports = { update };
+const getAllInfoByRegistrationNumber = async (req, res) => {
+  const { numero_de_registro } = req.body;
+  const { _id, hierarquia } = req.body.payload;
+
+  if (hierarquia === "professor") {
+    return res.status(403).send({ message: "Acesso proibido." });
+  }
+
+  console.log(`Numero de registro: ${numero_de_registro}`);
+  console.log(`ID: ${_id}`);
+
+  try {
+    const aluno = await Aluno.findOne({
+      "conta._id": _id,
+      numero_de_registro,
+    });
+
+    if (!aluno) {
+      return res.status(400).send({
+        message:
+          "Somente o aluno com esse número de registro pode solicitar as informações.",
+      });
+    }
+
+    return res.status(200).send({
+      message: "Ok",
+      datas: {
+        email_de_contato: aluno.email_de_contato,
+        nome: aluno.nome,
+        sobrenome: aluno.sobrenome,
+        telefone: aluno.telefone,
+        numero_de_registro: aluno.numero_de_registro,
+        curso: aluno.curso,
+        turma: aluno.turma,
+        notas: aluno.notas.map((n) => {
+          return { materia: n.materia, nota: n.nota };
+        }),
+      },
+    });
+  } catch (err) {
+    return res.status(400).send({ message: err.message });
+  }
+};
+
+module.exports = {
+  update,
+  getAllInfoByRegistrationNumber /*, getContactInfoByRegistrationNumber, getCourseInfoByRegistrationNumber */,
+};
--- src/app/routes/aluno.js
+++ src/app/routes/aluno.js
@@ -5,4 +5,9 @@ const router = express.Router();
 
 router.post("/update", alunoController.update);
 
+router.get(
+  "/getAllInfoByRegistrationNumber",
+  alunoController.getAllInfoByRegistrationNumber
+);
+
 module.exports = router;
